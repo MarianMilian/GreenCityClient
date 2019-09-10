c1e754172be3855373fb6dc6e5c3919fe16d6bfe
studroma <studroma@gmail.com>
2019-09-10 09:48:46 +0300

    Fixed problems with path for /place/propose

--- src/app/service/place.service.ts
+++ src/app/service/place.service.ts
@@ -2,15 +2,13 @@ import {BaseService} from './base-service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {PlaceAddDto} from '../model/placeAddDto.model';
-import {Observable} from "rxjs";
-import {PlaceWithUserModel} from "../model/placeWithUser.model";
 
 @Injectable({providedIn: 'root'})
 export class PlaceService extends BaseService {
 
   constructor(protected http: HttpClient) {
     super(http);
-    this.apiUrl += '/place';
+    this.apiUrl += 'place';
   }
 
   save(place: PlaceAddDto) {
